aad32089a28c3e8ef400ea63ccda1bc467d0459a
瓜瓜 <2323398255@qq.com>
2024-07-31 23:16:34 +0800

    buffer

--- Buffer.cc
+++ Buffer.cc
@@ -1,10 +1,33 @@
-#include"Buffer.h"
+#include "Buffer.h"
+#include <sys/uio.h>
 
-
-Buffer::Buffer(/* args */)
+// 从fd上读取数据 Poller工作在LT模式
+// buffer缓冲区是有大小的！ 但是从fd上读取数据的时候，却不知道TCP数据最终的大小
+ssize_t Buffer::readFd(int fd, int *saveErrnp)
 {
-}
+    char extrabuf[65536] = {0}; // 栈上的内存 64k
+    struct iovec vec[2];
+    const size_t writeable = writeableBytes(); // Buffer底层缓冲区剩余的可写空间大小
+    vec[0].iov_base = begin() + wirteIndex_;
+    vec[0].iov_len = writeable;
+    vec[1].iov_base = extrabuf;
+    vec[1].iov_len = sizeof(extrabuf);
 
-Buffer::~Buffer()
-{
+    const int iovcnt = (writeable < sizeof extrabuf) ? 2 : 1;
+    const ssize_t n = ::readv(fd, vec, iovcnt);
+    if (n < 0)
+    {
+        *saveErrnp = errno;
+    }
+    else if (n <= writeable)  //buffer 的可写缓冲区已经狗存储读出来的数据了
+    {
+        wirteIndex_ += n;
+    }
+    else // extrabuf里面也写入了数据
+    {
+        wirteIndex_ = buffer_.size();
+        append(extrabuf, n - writeable); //writeIndex_开始写 n-writeabke大小的数据
+
+    }
+    return n;
 }
--- Buffer.h
+++ Buffer.h
@@ -1,11 +1,113 @@
 #pragma once
-
+#include <vector>
+#include <stddef.h>
+#include<string>
+#include<algorithm>
 class Buffer
 {
 public:
-    Buffer();
+    static const size_t kChreapPrepend = 8;
+    static const size_t kInitialSize = 1024;
+    explicit Buffer(size_t initalSize = kInitialSize)
+    : buffer_(kChreapPrepend + initalSize),
+    readerIndex_(kChreapPrepend),
+    wirteIndex_(kChreapPrepend)
+    {}
     ~Buffer();
+    size_t readableBytes()const
+    {
+        return wirteIndex_ - readerIndex_;
+    }
+    size_t writeableBytes()const
+    {
+        return buffer_.size() - wirteIndex_;
+    }
+    size_t prependableBytes()const
+    {
+        return readerIndex_;
+    } 
 
+    //返回可读数据的起始地址
+    const char* peek()const 
+    {
+        return begin() + readerIndex_;
+    }
+
+    void retrieve(size_t len) //检索
+    {
+        if(len < readableBytes())
+        {
+            readerIndex_ += len;  //应用只读取了一部分len长度
+        }
+        else //len == readableBytes
+        {
+            retrieveAll();
+        }
+    }
+    void retrieveAll()
+    {
+        readerIndex_ = wirteIndex_ = kChreapPrepend;
+    }
+    //把onmessage函数上报的Buffer数据，转成string类型的数据返回
+    std::string retrieveAllAsString()
+    {
+        return retrieveAsstring(readableBytes());
+    }
+    std::string retrieveAsstring(size_t len)
+    {
+        std::string result(peek(),len);
+        retrieve(len);
+        return result;
+    }
+
+    void ensureWriteableBytes(size_t len)
+    {
+        if(writeableBytes() < len)
+        {
+            makeSpace(len);
+        }
+    }
+    //把[data,data+len] 内存上的数据，添加到writeable缓冲区当中
+    void append(const char* data , size_t len)
+    {
+        std::copy(data,data+len,beginWrite());
+        wirteIndex_ += len;
+    }
+    char* beginWrite()
+    {
+        return begin() + wirteIndex_;
+    }
+    const  char* beginWrite()const
+    {
+        return begin() + wirteIndex_;
+    }
+    //从fd上读取数据
+    ssize_t readFd(int fd , int * saveErrnp);
 private:
+    void makeSpace(size_t len)
+    {
+        if(writeableBytes() + prependableBytes() < len + kChreapPrepend)
+        {
+            buffer_.resize(wirteIndex_+len);
+        }
+        else
+        {
+            size_t readable = readableBytes();
+            std::copy(begin() + readerIndex_, begin() + wirteIndex_,begin() + kChreapPrepend);
+            readerIndex_ = kChreapPrepend;
+            wirteIndex_ = readerIndex_ + readable;
+        }
+    }
 
+    char* begin()
+    {
+        return &*buffer_.begin();
+    }
+    const char* begin()const
+    {
+        return &*buffer_.begin();
+    }
+    std::vector<char> buffer_;
+    size_t readerIndex_;
+    size_t wirteIndex_;
 };
